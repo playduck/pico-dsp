e7f129bd1e22fdd75c172ac9c3391ea6b596cec3
robin <robin.prillwitz@gmail.com>
2022-08-28 15:33:56 +0200

    renamed dsp to main

--- main.cpp
+++ main.cpp
@@ -38,9 +38,7 @@ mutex_t _pioMutex;
 
 int __not_in_flash_func(main)()
 {
-    stdio_init_all();
-
-    // binary info
+    /* binary info */
     bi_decl(bi_program_description("pico-dsp - a simple audio dsp"));
     bi_decl(bi_1pin_with_name(input_BCLK_Base, "I2S Input (ADC) BCLK, +1 is LRCK - DON'T USE (invalid timing)"));
     bi_decl(bi_1pin_with_name(input_DATA, "I2S Input (ADC) Data"));
@@ -48,24 +46,26 @@ int __not_in_flash_func(main)()
     bi_decl(bi_1pin_with_name(output_DATA, "I2S Output (DAC) Data"));
     bi_decl(bi_1pin_with_name(mclk_pin, "I2S MCLK"));
 
+    /* start uart */
+    stdio_init_all();
+
+    /* on-board led */
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
-
-    // PIOs wait for IRQ7, so enable before loading
+    /* PIOs wait for IRQ7, so enable before loading */
     irq_set_enabled(7, true);
 
-    // undervolt core voltage
-    // might enable higher clock speeds (smaller voltage slopes)
-    vreg_set_voltage(VREG_VOLTAGE_1_00);
-    sleep_ms(1000); // vreg settle
 
-    // overclock system
+    /* undervolt core voltage
+        might enable higher clock speeds */
+    // vreg_set_voltage(VREG_VOLTAGE_1_00);
+    // sleep_ms(1000); // vreg settle
+
+    /* overclock system */
     // set_sys_clock_khz(230000, true);
-    sleep_ms(100);
-
-    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+    // sleep_ms(100);
 
     I2S I2S_Output(OUTPUT);
     I2S I2S_Input(INPUT);
@@ -77,19 +77,21 @@ int __not_in_flash_func(main)()
 
     IIR shaping1(peak, 80, BIQUAD_Q_ORDER_2, 6.0, sampleRate);
 
-    // load mclk pio
+    /* load mclk pio */
     int off = 0, sm = 0;
     PIO pio;
-    PIOProgram* mclk = new PIOProgram(&pio_i2s_mclk_program);
-    if(mclk->prepare(&pio, &sm, &off))  {
+    PIOProgram* mclkPio = new PIOProgram(&pio_i2s_mclk_program);
+    if(mclkPio->prepare(&pio, &sm, &off))  {
         pio_i2s_mclk_program_init(pio, sm, off, mclk_pin);
         // set mclk to a multiple of fs
         pio_sm_set_clkdiv(pio, sm, (float)clock_get_hz(clk_sys) / (mclkFactor  * (float)sampleRate));
         printf("allocated MCLK PIO\n");
     }   else    {
         printf("failed to allocate MCLK PIO\n");
+        while(1);
     }
 
+    /* initilize I2S */
     I2S_Input.setBCLK(input_BCLK_Base);
     I2S_Input.setDATA(input_DATA);
     I2S_Output.setBCLK(output_BCLK_Base);
@@ -98,12 +100,12 @@ int __not_in_flash_func(main)()
     I2S_Input.setBitsPerSample(bitDepth);
     I2S_Output.setBitsPerSample(bitDepth);
 
-    // increase buffer sizes
-    I2S_Input.setBuffers(32, 64, 0);
-    I2S_Output.setBuffers(32, 64, 0);
-
     I2S_Input.setFrequency(sampleRate);
     I2S_Output.setFrequency(sampleRate);
+
+    /* increase I2S buffer sizes */
+    I2S_Input.setBuffers(32, 64, 0);
+    I2S_Output.setBuffers(32, 64, 0);
 
     if (!I2S_Output.begin())
     {
@@ -117,33 +119,36 @@ int __not_in_flash_func(main)()
         while (1);
     }
 
-    printf("entering main loop\n");
-
+    /* loop variables */
     int32_t left_rx = 0, right_rx = 0;
     int32_t left_tx = 0, right_tx = 0;
 
-    // sync all pio0 clocks
+    printf("entering main loop\n");
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+
+    /* sync all pio0 clocks */
     pio_enable_sm_mask_in_sync(pio0, 0xF);
-    // start PIOs in sync using falling edge of IRQ7
+    /* start PIOs in sync using falling edge of IRQ7 */
     irq_set_enabled(7, false);
 
     while (1)
     {
         I2S_Input.read32(&left_rx, &right_rx);
 
-        // scale 24 Bit sample to range
+        /* scale 24 Bit sample to range */
         left_tx = left_rx >> 8;
         right_tx = right_rx >> 8;
 
-        lowpass1.filter(&left_tx);
-        lowpass2.filter(&left_tx);
-        shaping1.filter(&left_tx);
+        lowpass1.filter(&left_tx); // +0dB
+        lowpass2.filter(&left_tx); // +0dB
+        shaping1.filter(&left_tx); // +6dB
 
-        highpass1.filter(&right_tx);
-        highpass2.filter(&right_tx);
+        highpass1.filter(&right_tx); // +0dB
+        highpass2.filter(&right_tx); // +0dB
 
-        // makeup gain
-        // should be 8, leave 1 bit headroom
+        /* makeup gain
+            +6dB max -> scale by 2^1
+            headroom is 2^8 - 2^1 -> 2^7 */
         left_tx <<= 7;
         right_tx <<= 7;
 
